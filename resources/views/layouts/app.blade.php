a10984c7ed8ea65cc0daf40a294925f88eb00a08
Younes-Belaabda <belaabda.youness@gmail.com>
2024-05-07 16:21:08 +0100

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,23 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CodeGenerator v.1</title>
-    @vite(['resources/css/app.css' , 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <aside>
-        <ul>
-            <li>
-                <a href="{{ route('codes.create') }}">Generate Codes</a>
-            </li>
-            <li>
-                <a href="{{ route('codes.index') }}">Codes Database</a>
-            </li>
-        </ul>
-    </aside>
-    <main>
-        {{ $slot }}
-    </main>
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x">
+            <div class="medium-2 cell">
+                <aside>
+                    <ul>
+                        <li>
+                            <a href="{{ route('codes.create') }}">Generate Codes</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('codes.index') }}">Codes Database</a>
+                        </li>
+                    </ul>
+                </aside>
+            </div>
+            <div class="medium-10 cell">
+                <main>
+                    {{ $slot }}
+                </main>
+            </div>
+        </div>
+    </div>
 </body>
 
 <script src="{{ asset('assets/clipboard.js') }}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}" @if(config('app.locale') == 'ar') dir="rtl" @endif>
+<html lang="{{ config('app.locale') }}" @if (config('app.locale') == 'ar') dir="rtl" @endif>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@lang('master.title')</title>
-    @vite(['resources/css/app.css' , 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -19,7 +19,27 @@
                 <a href="#">BL97</a>
             </p>
         </div>
-        <a class="button" href="{{ route('login') }}">@lang('auth.login')</a>
+        <div class="callout">
+            <h5>تسجيل الدخول</h5>
+            <hr>
+            <form>
+                <div class="grid-container">
+                    <div class="grid-x grid-padding-x">
+                        <div class="medium-12 cell">
+                            <label>@lang('auth.email')
+                                <input type="text" name="email" placeholder="@lang('auth.email')">
+                            </label>
+                        </div>
+                        <div class="medium-12 cell">
+                            <label>@lang('auth.password')
+                                <input type="password" name="password" placeholder="@lang('auth.password')">
+                            </label>
+                        </div>
+                    </div>
+                    <button class="button">@lang('auth.login')</button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 
