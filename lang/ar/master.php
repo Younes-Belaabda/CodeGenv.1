8c6695f1a5665034cee76b1242f435f4f1cf2540
Younes-Belaabda <belaabda.youness@gmail.com>
2024-05-07 16:27:52 +0100

    wip

--- lang/ar/master.php
+++ lang/ar/master.php
@@ -3,5 +3,9 @@
     return [
         'title' => 'موقع إنشاء الأكواد',
         'description' => 'إنشاء مجموعة من الأكواد و توليد جميع الإحتمالات الممكنة',
-        'submit' => 'موافقة'
+        'submit' => 'موافقة',
+        'hashcode_prefix' => 'الكلمة قبل الكود',
+        'hashcode_suffix' => 'الكلمة بعد الكود',
+        'hashcode_length' => 'طول الكود',
+        'hashcode_range' => 'أحرف الكود',
     ];
--- resources/views/codes/create.blade.php
+++ resources/views/codes/create.blade.php
@@ -4,27 +4,27 @@
         <div class="grid-container">
             <div class="grid-x grid-padding-x">
                 <div class="medium-12 cell">
-                    <label>Prefix
-                        <input type="text" name="prefix" placeholder="prefix code">
+                    <label>@lang('master.hashcode_prefix')
+                        <input type="text" name="prefix" placeholder="@lang('master.hashcode_prefix')">
                     </label>
                 </div>
                 <div class="medium-12 cell">
-                    <label>Suffix
-                        <input type="text" name="suffix" placeholder="suffix code">
+                    <label>@lang('master.hashcode_suffix')
+                        <input type="text" name="suffix" placeholder="@lang('master.hashcode_suffix')">
                     </label>
                 </div>
                 <div class="medium-12 cell">
-                    <label>Length *
-                        <input type="text" name="length" placeholder="length code">
+                    <label>@lang('master.hashcode_length') *
+                        <input type="text" name="length" placeholder="@lang('master.hashcode_length')">
                     </label>
                 </div>
                 <div class="medium-12 cell">
-                    <label>Range
-                        <input type="text" name="range" placeholder="range">
+                    <label>@lang('master.hashcode_range')
+                        <input type="text" name="range" placeholder="@lang('master.hashcode_range')">
                     </label>
                 </div>
             </div>
-            <button class="button">Submit</button>
+            <button class="button">@lang('master.submit')</button>
         </div>
     </form>
 </x-app-layout>
