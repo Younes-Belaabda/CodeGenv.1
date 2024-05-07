a74c052014a406bcb311c6a64a0041f736145d18
Younes-Belaabda <belaabda.youness@gmail.com>
2024-05-07 16:28:26 +0100

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ config('app.locale') }}" @if(config('app.locale') == 'ar') dir="rtl" @endif>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CodeGenerator v.1</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>@lang('master.title')</title>
+    @vite(['resources/css/app.css' , 'resources/js/app.js'])
 </head>
 
 <body>
