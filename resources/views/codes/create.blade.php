09745940b3965b1c495f3e4c9b885334fbf4af65
Younes-Belaabda <belaabda.youness@gmail.com>
2024-05-08 13:41:06 +0100

    wip

--- resources/views/codes/create.blade.php
+++ resources/views/codes/create.blade.php
@@ -1,45 +1,47 @@
 <x-app-layout>
     <form action="{{ route('codes.store') }}" method="POST">
         @csrf
-        <div class="grid-container">
-            <div class="grid-x grid-padding-x">
-                <div class="medium-12 cell">
-                    <label>@lang('master.hashcode_group')
-                        <input type="text" name="group" placeholder="@lang('master.hashcode_group')">
-                    </label>
+        <div class="callout secondary">
+            <div class="grid-container">
+                <div class="grid-x grid-padding-x">
+                    <div class="medium-12 cell">
+                        <label>@lang('master.hashcode_group')
+                            <input type="text" name="group" placeholder="@lang('master.hashcode_group')">
+                        </label>
+                    </div>
+                    <div class="medium-6 cell">
+                        <label>@lang('master.hashcode_prefix')
+                            <input type="text" name="prefix" placeholder="@lang('master.hashcode_prefix')">
+                        </label>
+                    </div>
+                    <div class="medium-6 cell">
+                        <label>@lang('master.hashcode_suffix')
+                            <input type="text" name="suffix" placeholder="@lang('master.hashcode_suffix')">
+                        </label>
+                    </div>
+                    {{-- <div class="medium-12 cell">
+                        <label>@lang('master.hashcode_length') *
+                            <input type="text" name="length" placeholder="@lang('master.hashcode_length')">
+                        </label>
+                    </div> --}}
+                    <div class="medium-4 cell">
+                        <label>@lang('master.hashcode_smallcase')
+                            <input type="text" name="smallcase" placeholder="@lang('master.hashcode_smallcase')">
+                        </label>
+                    </div>
+                    <div class="medium-4 cell">
+                        <label>@lang('master.hashcode_uppercase')
+                            <input type="text" name="uppercase" placeholder="@lang('master.hashcode_uppercase')">
+                        </label>
+                    </div>
+                    <div class="medium-4 cell">
+                        <label>@lang('master.hashcode_numbers')
+                            <input type="text" name="numbers" placeholder="@lang('master.hashcode_numbers')">
+                        </label>
+                    </div>
                 </div>
-                <div class="medium-6 cell">
-                    <label>@lang('master.hashcode_prefix')
-                        <input type="text" name="prefix" placeholder="@lang('master.hashcode_prefix')">
-                    </label>
-                </div>
-                <div class="medium-6 cell">
-                    <label>@lang('master.hashcode_suffix')
-                        <input type="text" name="suffix" placeholder="@lang('master.hashcode_suffix')">
-                    </label>
-                </div>
-                {{-- <div class="medium-12 cell">
-                    <label>@lang('master.hashcode_length') *
-                        <input type="text" name="length" placeholder="@lang('master.hashcode_length')">
-                    </label>
-                </div> --}}
-                <div class="medium-4 cell">
-                    <label>@lang('master.hashcode_smallcase')
-                        <input type="text" name="smallcase" placeholder="@lang('master.hashcode_smallcase')">
-                    </label>
-                </div>
-                <div class="medium-4 cell">
-                    <label>@lang('master.hashcode_uppercase')
-                        <input type="text" name="uppercase" placeholder="@lang('master.hashcode_uppercase')">
-                    </label>
-                </div>
-                <div class="medium-4 cell">
-                    <label>@lang('master.hashcode_numbers')
-                        <input type="text" name="numbers" placeholder="@lang('master.hashcode_numbers')">
-                    </label>
-                </div>
+                <button class="button">@lang('master.submit')</button>
             </div>
-            <button class="button">@lang('master.submit')</button>
         </div>
     </form>
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,5 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot> --}}
-    Content
+    <div class="callout warning">
+        @lang('master.description')
+    </div>
 </x-app-layout>
